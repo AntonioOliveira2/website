ca74d75327fedd01db224c421ecccc24f63e7018
Antonio Oliveira <antonio.oliveira5487@gmail.com>
2024-12-28 00:03:07 +0000

    new start in reactjs-remix, navbar and logo redone

---
+++ app/routes/general/navbar.tsx
@@ -0,0 +1,40 @@
+import { Link, useLocation } from "@remix-run/react";
+import { useState } from "react";
+
+export default function Navbar() {
+    const location = useLocation();
+    const showHomeButton = !(location.pathname === "/");
+    
+    const homeButton = (
+        <li className="mr-6">
+            <Link to="/" className="text-base text-white hover:text-gray-400">Home</Link>
+        </li>
+    );
+
+    return (
+        <nav className="bg-gray-800">
+            <div className="py-3 flex">
+                <div className="pl-5 justify-left items-left">
+                    <Link to="/">
+                        <img className="invert w-10 hover:scale-125 transform transition duration-250" src="/logo.svg" alt="" />
+                    </Link>
+                </div>
+                <div className="flex items-center justify-center w-full">
+                    <ul className="flex">
+                        {showHomeButton && homeButton}
+                        <li className="mr-6">
+                            <Link to="/material/fatima-concelho" className="text-base text-white hover:text-gray-400">Fatima Concelho</Link>
+                        </li>
+                        <li className="mr-6">
+                            <Link to="/" className="text-base text-white hover:text-gray-400">Link</Link>
+                        </li>
+                        <li className="mr-6">
+                        <Link to="/" className="text-base text-white hover:text-gray-400">Another Link</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+  }
+  
---
+++ app/routes/material.fatima-concelho.tsx
@@ -0,0 +1,25 @@
+import parse from 'html-react-parser';
+import { useEffect, useState } from 'react';
+
+
+export default function FatimaConcelho() {
+
+    const [htmlFileString, setHtmlFileString] = useState("");
+
+    async function fetchHtml() {
+        const response = await fetch('/estatistica.html');
+        const res = await response.text();
+
+        if (res) {
+            setHtmlFileString(res);
+        }
+    }
+
+    useEffect(() => {
+      fetchHtml();
+    }, []);
+
+    return (
+        parse(htmlFileString)
+    );
+}
