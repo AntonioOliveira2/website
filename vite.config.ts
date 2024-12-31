e8a23c8a5efbd1cf4cf6a603a15d20ff3278733d
Antonio Oliveira <antonio.oliveira5487@gmail.com>
2024-12-31 20:43:59 +0000

    corrected config to generate static html

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   plugins: [
     remix({
       basename: '/website/',
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
